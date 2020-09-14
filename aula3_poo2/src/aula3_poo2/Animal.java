f029ec429f7337e7c0b836e9510e51be388fd587
Belmires <belmires.10@hotmail.com>
2020-09-14 19:45:52 -0300

    Segundo

---
+++ aula3_poo2/src/aula3_poo2/Animal.java
@@ -0,0 +1,9 @@
+package aula3_poo2;
+
+public class Animal {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+	}
+
+}
--- aula3_poo2/src/aula3_poo2/HelloWord.java
+++
@@ -1,5 +0,0 @@
-package aula3_poo2;
-
-public class HelloWord {
-
-}
