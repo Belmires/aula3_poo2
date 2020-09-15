9d0a79b28b2753edca6596ed390f4fda2b85768f
Belmires <belmires.10@hotmail.com>
2020-09-14 21:02:40 -0300

    exercicio02

---
+++ aula3_poo2/src/aula3_poo2/Banco.java
@@ -0,0 +1,5 @@
+package aula3_poo2;
+
+public class Banco {
+
+}
---
+++ aula3_poo2/src/aula3_poo2/Cadeira.java
@@ -0,0 +1,48 @@
+package aula3_poo2;
+
+public class Cadeira {
+	
+	String tamanho;
+	
+
+	Double peso;
+	
+	String cor;
+	
+	Cadeira(){
+		
+	}
+
+	public String getTamanho() {
+		return tamanho;
+	}
+	
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+	
+	public Double getPeso() {
+		return peso;
+	}
+	
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	
+	public void inclinar() {
+		
+	}
+	
+	public void massagear(double graus) {
+		
+	}
+}
